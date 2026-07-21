a1dc630b4d904c2fa726ba2c2ffe659d57ddf674
“JavierEM” <jembfh@gmail.com>
2026-07-21 10:04:41 +0200

    testing postman ok

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import express from "express";
 import cors from "cors";
 import clientsRouter from "./routes/clientsRoutes.js"
 import { connectDB } from "./config/db.js";
-const app = express();
 
-await connectDB();
+const app = express();
+app.use(express.json());
 app.use(cors());
+await connectDB();
+
 app.use("/clients",clientsRouter);
 app.listen(3000, () => {
   console.log("servidor iniciado");
