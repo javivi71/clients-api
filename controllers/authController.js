e3b03b13e6690c07d0a140fa81852510fdcf28fa
“JavierEM” <jembfh@gmail.com>
2026-07-23 12:29:02 +0200

    fase 4 complete

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,6 +45,9 @@ export const login = async (req, res) => {
       token,
     });
 
+   console.log(jwt.decode(token));
+   
+
   } catch (error) {
     res.status(500).json({
       message: error.message,
